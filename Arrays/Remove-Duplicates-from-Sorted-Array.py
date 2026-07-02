cf5468a98f09c32baa08510a58d377c3d7fffef3
Kulkarni Srushti <69637158+Srushti750@users.noreply.github.com>
2026-07-02 17:49:12 +0530

    Delete Arrays directory

--- Arrays/Remove-Duplicates-from-Sorted-Array.py
+++
@@ -1,9 +0,0 @@
-1class Solution:
-2    def removeDuplicates(self, nums: List[int]) -> int:
-3        i = 0
-4        while i < len(nums)-1:
-5            if(nums[i] == nums[i+1]):
-6                nums.pop(i+1)
-7            else:
-8                i += 1
-9        return len(nums)
