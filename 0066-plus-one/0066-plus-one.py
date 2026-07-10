d28ab7232b31b473533666d41e37221f6478e751
Kulkarni Srushti <69637158+Srushti750@users.noreply.github.com>
2026-07-10 17:02:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.72%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = 0
+        for i in digits:
+            num = num * 10 + i
+        num = num + 1
+        result = list(map(int, str(num)))
+        return result
