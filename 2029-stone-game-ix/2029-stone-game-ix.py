560a229df43575801270157071aaf9179958801c
Srushti Kulkarni <69637158+Srushti750@users.noreply.github.com>
2026-08-16 18:40:29 +0530

    Time: 41 ms (93.13%), Space: 30.5 MB (61.07%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        count = [0, 0, 0]
+        for i in stones:
+            count[i%3] += 1
+        
+        count0, count1, count2 = count
+
+        if count0 % 2 == 0:
+            return count1>0 and count2>0
+        return abs(count1 - count2) > 2
